716825f9d365ea80bb7f77628badff26ee510c98
Chris Kitras <chkitras@gmail.com>
2020-12-21 01:52:26 -0700

    Uptime completed

--- tiny pc/Tiny_Status/status.py
+++ tiny pc/Tiny_Status/status.py
@@ -25,41 +25,14 @@ UPTIME_TIMER_MAX = 5
 MEM_TIMER_MAX = 5
 DISK_DELAY = 5
 
+# Format will be {'Name  ': [val, 1=percentage, 0=txt]}
+sysinfo = {}
 cpu_timer = 0
 uptime_timer = 0
 mem_timer = 0
-
-# Get number of CPUs on system
-cpusinfo = subprocess.Popen(
-    ('cat', '/proc/cpuinfo'), stdout=subprocess.PIPE)
-greplast = subprocess.Popen(
-    ('grep', 'processor'), stdin=cpusinfo.stdout, stdout=subprocess.PIPE)
-cpusinfo.stdout.close()
-taillast = subprocess.check_output(
-    ('tail', '-1'), stdin=greplast.stdout)
-greplast.stdout.close()
-string = taillast.decode('utf-8')
-num_of_cpu = int(string.split()[2]) + 1  # Add to compensate for zero index
-
-# Obtain advertised speed of CPU
-lscpuinfo = subprocess.Popen(('lscpu'), stdout=subprocess.PIPE)
-grepspeed = subprocess.check_output(
-    ('grep', 'CPU max MHz'), stdin=lscpuinfo.stdout)
-lscpuinfo.stdout.close()
-string2 = grepspeed.decode('utf-8')
-max_cpu_speed = int(float(string2.split()[3]))
-
-# Arch of CPU
-lscpuarch = subprocess.Popen(('lscpu'), stdout=subprocess.PIPE)
-arch_grep = subprocess.check_output(
-    ('grep', 'Architecture'), stdin=lscpuarch.stdout)
-lscpuarch.stdout.close()
-arch_str = arch_grep.decode('utf-8')
-arch_str = arch_str.rstrip()
-arch_str = arch_str.split()[1]
-
-# Format will be {'Name  ': [val, 1=percentage, 0=txt]}
-sysinfo = {}
+num_of_cpu = 0
+max_cpu_speed = 0
+arch_str = ''
 
 
 def fetch_mem():
@@ -78,7 +51,7 @@ def fetch_cpu():
     # Check to see if there are more than 4 CPUs (zero indexed)
 
     # Too big for screen, only report average
-    if num_of_cpu > 10 + MAX_LINE:
+    if num_of_cpu > MAX_LINE:
         cpu_mhz_line = subprocess.Popen(
             ('lscpu'), stdout=subprocess.PIPE)
         grep_cpu_load = subprocess.check_output(
@@ -116,8 +89,58 @@ def fetch_cpu():
 
 
 def fetch_uptime():
-    print("uptime")
-    title = "uptime"
+    has_day = False
+    has_hour = False
+    has_minute = False
+    uptimelist = subprocess.check_output(('uptime', '-p'))
+    uptimelist = uptimelist.decode('utf-8')
+    uptimelist = uptimelist.rstrip()
+    uptimelist = re.sub("[^\w\s]", "", uptimelist)
+    uptimelist = uptimelist.split()
+    for item in uptimelist:
+        if item.__contains__('day'):
+            has_day = True
+        elif item.__contains__('hour'):
+            has_hour = True
+        elif item.__contains__('minute'):
+            has_minute = True
+
+    if has_day:
+        day_ind = -1
+        try:
+            day_ind = uptimelist.index('day') - 1
+        except ValueError:
+            day_ind = uptimelist.index('days') - 1
+        day = uptimelist[day_ind]
+    else:
+        day = 0
+
+    if has_hour:
+        hour_ind = -1
+        try:
+            hour_ind = uptimelist.index('hour') - 1
+        except ValueError:
+            hour_ind = uptimelist.index('hours') - 1
+        hour = uptimelist[hour_ind]
+    else:
+        hour = 0
+
+    if has_minute:
+        minute_ind = -1
+        try:
+            minute_ind = uptimelist.index('minute') - 1
+        except ValueError:
+            minute_ind = uptimelist.index('minutes') - 1
+        minute = uptimelist[minute_ind]
+    else:
+        minute = 0
+
+    sysinfo['Uptime'] = ['', 0]
+    sysinfo['Day   '] = [day, 0]
+    sysinfo['Hour  '] = [hour, 0]
+    sysinfo['Minute'] = [minute, 0]
+
+    print(sysinfo)
     time.sleep(1)
 
 
@@ -128,6 +151,39 @@ def to_serial():
 
 def update_init():
     global CUR_STATE
+    global max_cpu_speed
+    global arch_str
+    global num_of_cpu
+
+    # Get number of CPUs on system
+    cpusinfo = subprocess.Popen(
+        ('cat', '/proc/cpuinfo'), stdout=subprocess.PIPE)
+    greplast = subprocess.Popen(
+        ('grep', 'processor'), stdin=cpusinfo.stdout, stdout=subprocess.PIPE)
+    cpusinfo.stdout.close()
+    taillast = subprocess.check_output(
+        ('tail', '-1'), stdin=greplast.stdout)
+    greplast.stdout.close()
+    string = taillast.decode('utf-8')
+    num_of_cpu = int(string.split()[2]) + 1  # Add to compensate for zero index
+
+    # Obtain advertised speed of CPU
+    lscpuinfo = subprocess.Popen(('lscpu'), stdout=subprocess.PIPE)
+    grepspeed = subprocess.check_output(
+        ('grep', 'CPU max MHz'), stdin=lscpuinfo.stdout)
+    lscpuinfo.stdout.close()
+    string2 = grepspeed.decode('utf-8')
+    max_cpu_speed = int(float(string2.split()[3]))
+
+    # Arch of CPU
+    lscpuarch = subprocess.Popen(('lscpu'), stdout=subprocess.PIPE)
+    arch_grep = subprocess.check_output(
+        ('grep', 'Architecture'), stdin=lscpuarch.stdout)
+    lscpuarch.stdout.close()
+    arch_str = arch_grep.decode('utf-8')
+    arch_str = arch_str.rstrip()
+    arch_str = arch_str.split()[1]
+
     CUR_STATE = 1
 
 
